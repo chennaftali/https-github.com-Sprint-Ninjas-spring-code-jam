f1a96445a1bd9027d2ed4ce33b9ace26c17b0a34
kerwindows <kerwindows@hotmail.com>
2022-05-25 14:28:25 -0400

    added project.html page and gallery

--- scripts/index.js
+++ scripts/index.js
@@ -40,3 +40,26 @@ function displayMenu() {
         menu.style.display = 'none';
     }
 }
+
+
+//Image gallery
+let list = document.querySelectorAll('.gallery__list');
+let itemBox = document.querySelectorAll('.gallery__item');
+for(let i = 0; i<list.length; i++){
+    list[i].addEventListener('click', function(){
+        for(let j = 0; j<list.length; j++){
+            list[j].classList.remove('gallery__list_active');
+        }
+        this.classList.add('gallery__list_active');
+        let dataFilter = this.getAttribute('data-filter');
+        for(let k = 0; k<itemBox.length; k++){
+            itemBox[k].classList.remove('gallery__list_active');
+            itemBox[k].classList.add('gallery__list_hide');
+        
+        if(itemBox[k].getAttribute('data-item') == dataFilter || dataFilter == "all"){
+            itemBox[k].classList.remove('gallery__list_hide');
+            itemBox[k].classList.add('gallery__list_active');
+        }
+    }
+    })
+}
