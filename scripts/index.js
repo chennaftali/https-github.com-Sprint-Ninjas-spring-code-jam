71dc1d4b4dca65aec35a0f8fb78317b10d5d99f6
kerwindows <kerwindows@hotmail.com>
2022-05-24 20:44:37 -0400

    made changes to header and footer. Added responsive mobile menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,42 @@
-
+//Terry Blinking on desktop view
 const eyes = document.querySelector(".profile__hero-image-eyes");
+
+//set interval
 function startBlinking() {
-    setInterval(blinking,5500);
- }
- 
- function blinking() {
-   
+    setInterval(blinking, 5500);
+}
+
+function blinking() {
+
     eyes.style.opacity = 1;
-     
-       setTimeout(function(){
+
+    setTimeout(function() {
         eyes.style.opacity = 0;
-       },200);
- }
- startBlinking();
+    }, 200);
+}
+startBlinking();
+
+
+//responsive menu
+const menuButton = document.querySelector(".nav__menu");
+const menu = document.querySelector(".nav__links");
+
+function toggleMenu() {
+    menu.style.display == "flex" ? menu.style.display = "none" :
+        menu.style.display = "flex";
+}
+menuButton.addEventListener('click', toggleMenu);
+
+
+
+//display or hide menu when screen resizes 
+displayMenu();
+window.addEventListener('resize', displayMenu);
+
+function displayMenu() {
+    if (document.documentElement.clientWidth > 788) {
+        menu.style.display = 'flex';
+    } else {
+        menu.style.display = 'none';
+    }
+}
